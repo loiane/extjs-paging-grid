4509701ffb3aa1939f22560c47f7cbd426142394
loiane <loiane@10.150.14.137>
2010-08-31 16:57:35 -0300

    first commit

---
+++ src/com/loiane/dao/ContactDAO.java
@@ -0,0 +1,58 @@
+package com.loiane.dao;
+
+import java.util.List;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.DetachedCriteria;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.support.DataAccessUtils;
+import org.springframework.orm.hibernate3.HibernateTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.loiane.model.Contact;
+
+/**
+ * Contact DAO class
+ * 
+ * @author Loiane Groner
+ * http://loianegroner.com (English)
+ * http://loiane.com (Portuguese)
+ */
+@Repository
+public class ContactDAO implements IContactDAO{
+	
+	private HibernateTemplate hibernateTemplate;
+
+	@Autowired
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		hibernateTemplate = new HibernateTemplate(sessionFactory);
+	}
+	
+	
+	/**
+	 * Get List of contacts from database
+	 * @param start firt result to get from database
+	 * @param limit max of result to bring from database
+	 * @return list of contacts
+	 */
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Contact> getContacts(int start, int limit) {
+		
+		DetachedCriteria criteria = DetachedCriteria.forClass(Contact.class);
+		
+		return hibernateTemplate.findByCriteria(criteria, start, limit);
+		
+	}
+	
+	/**
+	 * Get total of Contacts from database
+	 * @return
+	 */
+	@Override
+	public int getTotalContacts(){
+		
+		return DataAccessUtils.intResult(hibernateTemplate.find("SELECT COUNT(*) FROM Contact"));
+	}
+
+}
